87661e56dd9857c65163baf79a1497d0a78a78c4
Igor <garikello@gmail.com>
2016-10-23 20:47:07 +0300

    Moved android-console test to netstats subfolder

--- tests/android-console/netstats/jni/Android.mk
+++ tests/android-console/netstats/jni/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := sysanal_console
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../../utility.cpp main.cpp
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../ $(LOCAL_PATH)/../../
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../utility.cpp main.cpp
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../../ $(LOCAL_PATH)/../../../
 LOCAL_SHARED_LIBRARIES := libsysanal
 
 include $(BUILD_EXECUTABLE)
 
-$(call import-add-path, $(LOCAL_PATH)/../../../)
+$(call import-add-path, $(LOCAL_PATH)/../../../../)
 $(call import-module, build/android/jni)
